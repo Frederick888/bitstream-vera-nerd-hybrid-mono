e5753630d106696bd12e72979c92c8bc9b4e1c9c
Frederick Zhang <frederick888@tsundere.moe>
2023-02-05 00:26:39 +1100

    Fix syntax of invalid block error

--- block_coverage.py
+++ block_coverage.py
@@ -14,7 +14,7 @@ class UnicodeBlock:
     def __init__(self, info: str):
         match = BLOCK_LINE_RE.match(info)
         if match is None:
-            raise "Invalid block"
+            raise RuntimeError(f"Invalid block {info}")
         self.start: int = int(match.group(1), 16)
         self.end: int = int(match.group(2), 16)
         self.name: str = match.group(3)
