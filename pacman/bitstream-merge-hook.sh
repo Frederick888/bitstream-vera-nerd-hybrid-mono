e175ed67e398835edad6d48467d3ef40807767a5
Frederick Zhang <frederick888@tsundere.moe>
2024-08-28 21:59:32 +1000

    Enable --monospace in pacman hook
    Under Arch Linux I didn't have the issue in [1], and it seemed to be
    harmless (so far) to use --monospace too. I probably mixed emojis in for
    Konsole, but now I mainly use Kitty, and maybe Konsole/fontconfig
    improved as well.
    
    [1] https://github.com/Frederick888/bitstream-vera-nerd-hybrid-mono/commit/eff3be32c38b2cefbf081556683a6e7a39ca6d4f

--- pacman/bitstream-merge-hook.sh
+++ pacman/bitstream-merge-hook.sh
@@ -7,6 +7,7 @@ while read -r trg; do
             --base '/usr/share/fonts/TTF/BitstromWeraNerdFontMono-Regular.ttf' \
             --output '/usr/local/share/fonts/Bitstream Vera Sans Mono Nerd Hybrid Font Complete Mono.ttf' \
             --categories 'Co,Cs,Sc,Sk,Sm,So' \
+            --monospace \
             'NotoEmoji-Regular.ttf::https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip' \
             'Symbola.otf::https://dn-works.com/wp-content/uploads/2020/UFAS-Fonts/Symbola.zip' 2>/dev/null
         ;;
